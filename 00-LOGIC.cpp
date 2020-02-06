31380c306540f510bb36c3603e06aa2e887c7fd0
laureano-dot <60376153+laureano-dot@users.noreply.github.com>
2020-02-06 20:29:45 +0800

    Create 00-LOGIC.cpp

---
+++ 00-LOGIC.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1,num2;
+	cout<<"Input num1"<<endl;
+	cin>>num1;
+	cout<<"Input num2"<<endl;
+	cin>>num1;
+			
+					  
+		    	 if(num1>=40&&num2<=40)
+ 						   cout<<"num1 and num2,one of them is greater than and equal to 40"<<endl;
+						    else
+ 						   cout<<"num1 and num2, one of them is not greater than and equal to 40"<<endl;
+ 						   
+ 						   if(num1>=40||num2>=40)
+ 						   cout<<"num1 and num2,one of them is greater than or equal to 40"<<endl;
+						    else
+ 						   cout<<"num1 and num2, one of them is not greater or and equal to 40"<<endl;
+ 						   
+ 						   
+ 		 			return 0;
+ }
+ 
