835a7f3b3c229db8308c5fbfe7a5bc8cde956d09
laureano-dot <60376153+laureano-dot@users.noreply.github.com>
2020-02-06 21:31:33 +0800

    Update 00-LOGIC.cpp

--- 00-LOGIC.cpp
+++ 00-LOGIC.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-	int num1,num2;
+	int num1,num2,num3,num4;
 	cout<<"Input num1"<<endl;
 	cin>>num1;
 	cout<<"Input num2"<<endl;
@@ -13,8 +13,12 @@ int main(){
  						   cout<<"num1 and num2,one of them is greater than and equal to 40"<<endl;
 						    else
  						   cout<<"num1 and num2, one of them is not greater than and equal to 40"<<endl;
- 						   
- 			 if(num1>=40||num2>=40)
+ 			
+	cout<<"enter num3:";		 			   
+     cin>>num3;
+     cout<<"enter num4";
+     cin>>num4;
+ 			 if(num3>=40||num4>=40)
  						   cout<<"num1 and num2,one of them is greater than or equal to 40"<<endl;
 						    else
  						   cout<<"num1 and num2, one of them is not greater or and equal to 40"<<endl;
